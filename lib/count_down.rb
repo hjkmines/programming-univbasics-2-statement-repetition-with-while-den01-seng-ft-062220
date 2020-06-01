9a27ac581c4b878d87c04268649d1366ae60485c
Hyung Kim <hjkmines@gmail.com>
2020-06-01 01:08:20 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,18 @@
 # Write your code here
+
+counter = 10 
+
+while counter >= 1 do 
+  
+  if counter == 1
+    puts "Happy New Year!"
+  end 
+  
+  puts counter 
+ 
+  counter -= 1
+  
+end 
+
+    
+  
